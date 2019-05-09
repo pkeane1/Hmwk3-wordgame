84d4ae942c4dfa6ff5c3ae7da31d61bee5f815fc
Pierce Keane <Pierce.Keane@gmail.com>
2019-05-09 17:48:46 -0400

    finsihing logic

--- template/Archive/logic.js
+++ template/Archive/logic.js
@@ -47,6 +47,8 @@ var round = {
  return round;
 }
 
+
+
 //1.6
 function updateRound(round,letter) {
     if (round.word.includes(letter)) {
@@ -57,3 +59,100 @@ function updateRound(round,letter) {
     }
     return round;
 }
+["h","_","l","l","_"]
+
+//1.7
+function hasWon(puzzleState) {
+
+    // if( !puzzleState.indexOf("_") === -1){
+    //     return true
+    // }
+    // return false
+
+   for (var i = 0; i < puzzleState.length; i++) {
+        if (puzzleState[i] === "_") {
+            return false;
+        }
+    }
+            return true;
+}
+
+//1.8- if out of lives you lose
+function hasLost(guessesLeft) {
+        if(guessesLeft > 0) {
+            return false;
+        } else { 
+            return true;
+        }
+    }     
+        
+//1.9
+function isEndOfRound(round) {
+    
+    
+    if (hasLost(round.guessesLeft) || hasWon(round.puzzleState)) {
+        return true;
+    } 
+     return false;
+    
+}
+//1.10 set up game
+
+function setupGame(words,wins,losses) { 
+    var game = {
+        words: words,
+        wins: wins,
+        losses:losses,
+        round:setupRound(randomWord(words))
+
+
+    }
+    return game;
+}
+
+    // randomWord(words.gameWords)
+    // hasWon(wins.puzzleState)
+    // hasLost(losses.guessesLeft)
+    
+   
+
+
+
+
+
+//1.11 start new round
+function startNewRound(game) {
+    if(hasWon(game.round.puzzleState)) {
+        alert(" winner " +  game.round.word)
+        game.wins++;
+
+    } else if(hasLost(game.round.guessesLeft)) {
+        alert(" Loser " + game.round.word)
+        game.losses++;
+    }
+    setupRound(randomWord(gameWords));
+}
+
+//1.12 
+var wins = 0
+var losses = 0
+var myGame = setupGame(gameWords,wins,losses);
+
+
+
+//
+// document.addEventListener("keyup", gamePlay)
+
+
+// function gamePlay(event){
+//     console.log(event.key)
+//     if (isCorrectGuess(myGame.round.word, event.key)){
+//         document.getElementById("puzzle-state").innerHTML =
+//     }
+// }
+
+document.getElementById("puzzle-state")
+puzzle.innerHTML = myGame.round.puzzleState.join(" ")
+
+
+ 
